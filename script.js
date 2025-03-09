653c88032c49da151ea2a16e02749bb73f2dc1ce
Bashie01 <aaron.jeffries2346@gmail.com>
2025-03-09 23:00:28 +0000

    Added game logic

---
+++ script.js
@@ -0,0 +1,68 @@
+// add a todo
+
+// list all todos
+
+// delete a todo
+
+// quit the app
+
+// while loop to ask and include a quit option, important to avoid infinite loop
+// prompt to ask what they would like to do
+// if they want to add a todo, prompt for the todo and add it to the list
+// if they want to list all todos, list all todos
+// if they want to delete a todo, prompt for the index of the todo and delete it
+// if they want to quit, break the loop
+
+// let input = prompt("What would you like to do?");
+// const todos = [];
+
+// while (input !== "quit" && input !== "q") {
+//   if (input === "list") {
+//     console.log("******");
+//     for (let i = 0; i < todos.length; i++) {
+//       console.log(`${i}: ${todos[i]}`);
+//     }
+//     console.log("******");
+//   } else if (input === "new") {
+//     const newTodo = prompt("Ok, what is the new todo?");
+//     todos.push(newTodo);
+//     console.log(`${newTodo} added to the list`);
+//   } else if (input === "delete") {
+//     const index = parseInt(prompt("Ok, what is the index to delete"));
+//     if (!Number.isNaN(index)) {
+//       const deleted = todos.splice(index, 1);
+//       console.log(`Ok, deleted ${delete [0]}`);
+//     } else {
+//       console.log("Unknown Index");
+//     }
+//   }
+//   input = prompt("What would you like to do?");
+// }
+// console.log("OK, quit the app");
+
+let input = prompt("Ok, what do you want to do");
+const todos = [];
+
+while (input !== "quit" && input !== "q") {
+  if (input === "list") {
+    console.log("*********");
+    for (i = 0; i < todos.length; i++) {
+      console.log(`${i}: ${todos[i]}`);
+    }
+    console.log("*********");
+  } else if (input === "new") {
+    const newTodo = prompt("Ok, what is the new todo");
+    todos.push(newTodo);
+    console.log(`Ok, ${newTodo} has been added to the list`);
+  } else if (input === "delete") {
+    const index = parseInt(prompt("Ok, to delete please type an index number"));
+    if (!Number.isNaN) {
+      const deleted = todos.splice(index, 1);
+      console.log(`Ok, ${delete [0]} has been deleted`);
+    }
+  } else {
+    console.log("Not a valid index");
+  }
+  input = prompt("ok, what do you want to do");
+}
+console.log("You have quit the app");
